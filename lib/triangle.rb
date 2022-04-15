939110fd1c1b93b687680d5ecd7f9d5b2dd09e57
Sean <craig.sean92@gmail.com>
2022-04-14 21:51:27 -0600

    commit tree 2015ecce3dae6f7fbf4108380c55ccb0a507a56f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  
+  attr_reader :kind
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @sides = [side1, side2, side3]
+   
+  end
+
+
+  def kind
+    if @sides.include?(0)
+      raise TriangleError
+    elsif @sides.any?{|num| num < 0}
+      raise TriangleError
+    elsif @sides.max >= @sides.sort.take(2).sum
+      raise TriangleError
+    end
+   
+
+    case @sides.uniq.length 
+    when 1
+      :equilateral
+    when 2
+      :isosceles
+    when 3
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "error"
+    end
+  end
+
 end
